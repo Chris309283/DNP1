ad66b97fa3ea485512cbac3138611ed95661236a
Chris309283 <80517220+Chris309283@users.noreply.github.com>
2022-02-14 22:22:15 +0100

    exercise4

--- S1_Ex1/Program.cs
+++ S1_Ex1/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+/*
+ var name = "Master";
+Console.WriteLine("Hello "+ name +"!");
+Console.WriteLine($"Hello {name}!");
+Console.WriteLine($"Hello {name.ToUpper()}!");
+*/
+
+var names = new[] {"Christopher", "Vlad", "Eliza", "Tomas"};
+foreach (var name in names)
+{
+ Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
---
+++ S1_Ex3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace S1_Ex3;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person p = new()
+        {
+            name = "Chris"
+        };
+        
+        p.Introduce();
+    }
+}
