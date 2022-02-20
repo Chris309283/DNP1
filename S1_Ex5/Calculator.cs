8e193c533d5b13d46c4ca77a5d0c6580290c2a5b
Chris309283 <80517220+Chris309283@users.noreply.github.com>
2022-02-20 23:19:41 +0100

    Finish s1 exercises

---
+++ S1_Ex5/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace S1_Ex5;
+public class Calculator
+{
+    public int Add(int x, int y)
+    {
+        return x + y; 
+    }
+    public int Add(int[] ints)
+    {
+        int sum = 0;
+
+        foreach (var variable in ints)
+        {
+            sum += variable;
+        }
+
+        return sum;
+    }
+
+    public int Max()
+    {
+        Console.WriteLine("Input first number: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Input second number: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        return Math.Max(x, y);
+    }
+}
